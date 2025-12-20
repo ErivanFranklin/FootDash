07f605f9db2003c9d50fd27b18fd14a12e0be130
Erivan Franklin da Silva <erivan.silva@ADMINS-IMAC.station>
2025-12-20 09:17:19 +0000

    fix: restore backend admin files

---
+++ backend/src/admin/admin.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../../users/user.entity';
+import { Report } from '../../social/entities/report.entity';
+import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User, Report])],
+  controllers: [AdminController],
+  providers: [AdminService],
+  exports: [AdminService],
+})
+export class AdminModule {}
